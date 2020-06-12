a3d8788aa38a825722bce2df044e5c169f846f33
padohy <padohy@DHpark>
2020-06-12 20:12:14 +0900

    int keyCode = System.in.read();

---
+++ ch04/src/sec03/ex01_keyCode/WhileKeyControl.java
@@ -0,0 +1,32 @@
+package sec03.ex01_keyCode;
+
+public class WhileKeyControl {
+	public static void main(String[] args) throws Exception {
+		boolean run = true;
+		int speed = 0;
+		int keyCode = 0;
+		
+		while(run) {
+			if(keyCode != 13 && keyCode != 10) {
+				System.out.println("--------------------");
+				System.out.println("1.증속 | 2.감속 | 3.중지");
+				System.out.println("--------------------");
+				System.out.print("선택: ");
+			}
+			keyCode = System.in.read();
+			
+			if(keyCode == 49) {
+				speed++;
+				System.out.println("현재 속도 : " + speed + "km/h");
+			} else if (keyCode == 50) { 
+				speed--;
+				System.out.println("현재 속도 : " + speed + "km/h");
+			} else if (keyCode == 51) {
+				run = false;
+			} 
+			
+		}
+		
+		System.out.println("프로그램 종료");
+	}
+}
