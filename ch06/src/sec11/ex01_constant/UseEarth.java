98d240110b2e7a9d6b83b572d46838ef0e74b7c8
padohy <padohy@DHpark>
2020-06-14 00:10:25 +0900

    상수(static final)

---
+++ ch06/src/sec11/ex01_constant/UseEarth.java
@@ -0,0 +1,9 @@
+package sec11.ex01_constant;
+
+public class UseEarth {
+	public static void main(String[] args) {
+		System.out.println("지구의 반지름 : " + Earth.EARTH_RADIUS + " km");
+		System.out.println("지구의 표현 : " + Earth.EARTH_SURFACE_AREA + " km^2");
+		
+	}
+}
