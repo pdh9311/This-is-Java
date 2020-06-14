45248cabca75fce2c148b30a6356431c2bf7ea43
padohy <padohy@DHpark>
2020-06-15 08:24:58 +0900

    (double)0.1 != (float)0.1f

---
+++ ch03/src/sec04_ex03_0dat1eq/CompareOperator0dot1.java
@@ -0,0 +1,9 @@
+package sec04_ex03_0dat1eq;
+
+public class CompareOperator0dot1 {
+	public static void main(String[] args) {
+		double v1 = 0.1;
+		float v2 = 0.1f;
+		System.out.println(v1 == v2);
+	}
+}
