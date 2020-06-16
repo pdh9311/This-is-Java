23ed2669ff0b063b4cbfe67693d58e827da9489b
padohy <padohy@DHpark>
2020-06-17 05:38:47 +0900

    익명 객체 UI 인벤트 처리

---
+++ ch09/src/sec05/ex03_UI이벤트처리/Button.java
@@ -0,0 +1,17 @@
+package sec05.ex03_UI이벤트처리;
+
+public class Button {
+	OnClickListener listener;
+	
+	void setOnClickListener(OnClickListener listener) {
+		this.listener = listener;
+	}
+	
+	void touch() {
+		listener.onClick();
+	}
+	
+	interface OnClickListener {
+		void onClick();
+	}
+}
---
+++ ch09/src/sec05/ex03_UI이벤트처리/UseWindow.java
@@ -0,0 +1,9 @@
+package sec05.ex03_UI이벤트처리;
+
+public class UseWindow {
+	public static void main(String[] args) {
+		Window window = new Window();
+		window.button1.touch();
+		window.button2.touch();
+	}
+}
