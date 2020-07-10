8b4e131768406259927f98e61a1918fa67316f51
padohy <padohy@DHpark>
2020-07-11 02:27:49 +0900

    0.0 <= Math.random < 1.0

--- ch04/src/sec02/ex01_mathRandom/MathRandom.java
+++ ch04/src/sec02/ex01_mathRandom/MathRandom.java
@@ -5,17 +5,17 @@ public class MathRandom {
 		// 0.0 <= Math.random() < 1.0
 		int random = (int) (Math.random()*6)+1;
 		if(random == 1) {
-			System.out.println("1 �Դϴ�.");
+			System.out.println("1 입니다.");
 		} else if (random == 2) {
-			System.out.println("2 �Դϴ�.");
+			System.out.println("2 입니다.");
 		} else if (random == 3) {
-			System.out.println("3 �Դϴ�.");
+			System.out.println("3 입니다.");
 		} else if (random == 4) {
-			System.out.println("4 �Դϴ�.");
+			System.out.println("4 입니다.");
 		} else if (random == 5) {
-			System.out.println("5 �Դϴ�.");
+			System.out.println("5 입니다.");
 		} else {
-			System.out.println("6 �Դϴ�.");
+			System.out.println("6 입니다.");
 		}
 		
 	}
